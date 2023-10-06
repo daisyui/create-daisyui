90c4e55a2cade84bd7a3902b60682d43b0ab9a7c
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-10-06 04:34:11 +0300

    update

---
+++ README.md
@@ -0,0 +1,35 @@
+# CLI to install and initiate daisyUI in your project
+
+For new or existing projects.
+
+## What it does?
+
+1. Installs Tailwind CSS and initializes it if needed
+2. Creates `tailwind.config.js` file if needed
+3. Installs daisyUI and adds it to your `tailwind.config.js` automatically
+
+## How to use?
+
+Run this in a new or existing project folder:
+
+```
+npm create daisyui
+```
+
+Or
+
+```
+npm init daisyui
+```
+
+Or if you use Bun:
+
+```
+bun create daisyui
+```
+
+Or if you use Yarn:
+
+```
+yarn create daisyui
+```
--- index.js
+++ index.js
@@ -1,82 +1,166 @@
 #! /usr/bin/env node
 
-const fs = require("fs");
-const parser = require("@babel/parser");
-const traverse = require("@babel/traverse").default;
-const generator = require("@babel/generator").default;
+import fs from "fs";
+import readline from "readline";
+import inquirer from "inquirer";
+import { execSync } from "child_process";
+import parser from "@babel/parser";
+import traverse from "@babel/traverse";
+import generator from "@babel/generator";
+import boxen from "boxen";
 
 const targetFile = "tailwind.config.js";
 const pluginToAdd = "daisyui";
 
-const targetCode = fs.readFileSync(targetFile, "utf-8");
-const ast = parser.parse(targetCode, { sourceType: "module" });
-
-let foundPluginsArray = false;
-
-traverse(ast, {
-  AssignmentExpression(path) {
-    if (
-      path.node.left.type === "MemberExpression" &&
-      path.node.left.object.name === "module" &&
-      path.node.left.property.name === "exports"
-    ) {
-      if (path.node.right.type === "ObjectExpression") {
-        const pluginsProperty = path.node.right.properties.find(
-          (prop) => prop.key.name === "plugins"
-        );
-        if (pluginsProperty) {
-          const pluginsArray = pluginsProperty.value;
-          if (pluginsArray.elements) {
-            const pluginNames = pluginsArray.elements.map(
-              (el) => el.arguments[0].value
-            );
-            if (!pluginNames.includes(pluginToAdd)) {
-              pluginsArray.elements.push(
-                parser.parse(`require("${pluginToAdd}")`).program.body[0]
-                  .expression
-              );
-            }
-          } else {
-            pluginsArray.elements = [
-              parser.parse(`require("${pluginToAdd}")`).program.body[0]
-                .expression,
-            ];
-          }
-        } else {
-          path.node.right.properties.push({
-            type: "ObjectProperty",
-            key: { type: "Identifier", name: "plugins" },
-            value: parser.parse(`[require("${pluginToAdd}")]`).program.body[0]
-              .expression,
-          });
-        }
-        foundPluginsArray = true;
-      }
-    }
-  },
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-if (!foundPluginsArray) {
-  const pluginsArrayNode = parser.parse(`plugins: [require("${pluginToAdd}")]`)
-    .program.body[0].expression;
-  const properties = ast.program.body[0].expression.right.properties;
-  if (properties.length > 0) {
-    const lastProperty = properties[properties.length - 1];
-    lastProperty.value = {
-      type: "ArrayExpression",
-      elements: [
-        parser.parse(`require("${pluginToAdd}")`).program.body[0].expression,
-      ],
-    };
-  } else {
-    properties.push({
-      type: "ObjectProperty",
-      key: { type: "Identifier", name: "plugins" },
-      value: pluginsArrayNode,
-    });
-  }
-}
+const addPluginToConfig = () => {
+  try {
+    const targetCode = fs.readFileSync(targetFile, "utf-8");
+    const ast = parser.parse(targetCode, { sourceType: "module" });
 
-const updatedCode = generator(ast, {}, targetCode).code;
-fs.writeFileSync(targetFile, updatedCode, "utf-8");
-console.log(`Added ${pluginToAdd} to ${targetFile}`);
+    let foundPluginsArray = false;
+
+    traverse.default(ast, {
+      AssignmentExpression(path) {
+        if (
+          path.node.left.type === "MemberExpression" &&
+          path.node.left.object.name === "module" &&
+          path.node.left.property.name === "exports"
+        ) {
+          if (path.node.right.type === "ObjectExpression") {
+            const pluginsProperty = path.node.right.properties.find(
+              (prop) => prop.key.name === "plugins"
+            );
+            if (pluginsProperty) {
+              const pluginsArray = pluginsProperty.value;
+              if (pluginsArray.elements) {
+                const pluginNames = pluginsArray.elements.map(
+                  (el) => el.arguments[0].value
+                );
+                if (!pluginNames.includes(pluginToAdd)) {
+                  pluginsArray.elements.push(
+                    parser.parse(`require("${pluginToAdd}")`).program.body[0]
+                      .expression
+                  );
+                }
+              } else {
+                pluginsArray.elements = [
+                  parser.parse(`require("${pluginToAdd}")`).program.body[0]
+                    .expression,
+                ];
+              }
+            } else {
+              path.node.right.properties.push({
+                type: "ObjectProperty",
+                key: { type: "Identifier", name: "plugins" },
+                value: parser.parse(`[require("${pluginToAdd}")]`).program
+                  .body[0].expression,
+              });
+            }
+            foundPluginsArray = true;
+          }
+        }
+      },
+    });
+
+    if (!foundPluginsArray) {
+      const pluginsArrayNode = parser.parse(
+        `plugins: [require("${pluginToAdd}")]`
+      ).program.body[0].expression;
+      const properties = ast.program.body[0].expression.right.properties;
+      if (properties.length > 0) {
+        const lastProperty = properties[properties.length - 1];
+        lastProperty.value = {
+          type: "ArrayExpression",
+          elements: [
+            parser.parse(`require("${pluginToAdd}")`).program.body[0]
+              .expression,
+          ],
+        };
+      } else {
+        properties.push({
+          type: "ObjectProperty",
+          key: { type: "Identifier", name: "plugins" },
+          value: pluginsArrayNode,
+        });
+      }
+    }
+
+    const updatedCode = generator.default(ast, {}, targetCode).code;
+    fs.writeFileSync(targetFile, updatedCode, "utf-8");
+
+    execSync("npm i -D daisyui@latest --silent", { stdio: "inherit" });
+    process.stdout.write("Installed daisyUI");
+
+    console.log(`\nAdded ${pluginToAdd} to ${targetFile}\n`);
+    console.log(`✅ Done.\n`);
+    console.log(`📘 How to use daisyUI: https://daisyui.com/docs/use/`);
+
+    if (!hadTailwindConfig) {
+      showHelp();
+    }
+  } catch (error) {
+    execSync("npm i -D daisyui@latest --silent", { stdio: "inherit" });
+    console.log("Installed daisyUI\n");
+    console.error(
+      `❌ Could not find ${targetFile} file.
+   Please config manually:
+   https://daisyui.com/docs/install/`
+    );
+  }
+};
+
+const showHelp = () => {
+  console.log("");
+  console.log(
+    boxen(
+      `📌 Things to do with a new Tailwind CSS setup:
+
+1. Add the paths to all template files in /tailwind.config.js:
+   content: ["./src/**/*.{html,js}"],\n
+2. Generate your CSS:
+   npx tailwindcss -o tailwind.css --watch\n
+3. Put the CSS in your HTML file:
+   <link rel="stylesheet" href="/tailwind.css" />
+
+Read more: https://tailwindcss.com/docs/installation'`,
+      { padding: 1, borderStyle: "round" }
+    )
+  );
+};
+
+console.log("\n⏳ Initializing daisyUI…\n");
+let hadTailwindConfig = false;
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "questionToSetupTailwind",
+      message: "Do you want to setup Tailwind CSS first?",
+      choices: [
+        "No need. I already have Tailwind",
+        "Yes. Setup Tailwind first",
+      ],
+    },
+  ])
+  .then((answers) => {
+    console.log("\n");
+    if (answers.questionToSetupTailwind === "Yes. Setup Tailwind first") {
+      execSync("npm i -D tailwindcss@latest --silent", { stdio: "inherit" });
+      process.stdout.write("Installed Tailwind CSS");
+
+      execSync("npx tailwindcss init", { stdio: "inherit" });
+
+      addPluginToConfig();
+    }
+    if (
+      answers.questionToSetupTailwind === "No need. I already have Tailwind"
+    ) {
+      hadTailwindConfig = true;
+      addPluginToConfig();
+    }
+  });
